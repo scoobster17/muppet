7d909fc78786cf3de91491fde12bd5ad15aa3e2c
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-07-12 09:35:59 +0100

    Added api key visibly and stopped worrying about hiding it

--- app/js/app.js
+++ app/js/app.js
@@ -7,69 +7,42 @@ muppet.map = function () {
 	var elems = {};
 	var config = {};
 
-	// private methods
+	/**************************************************************************/
+
+	/* PRIVATE METHODS */
+
+	// bind DOM elements to the elems object
 	var bindElems = function bindElems() {
 		elems.map = document.getElementById('map');
 		elems.key = document.getElementById('key');
 	};
 
-	var fetchApiKey = function fetchApiKey() {
+	var setupMap = function setupMap() {
 
-		console.log('ajax call to return key from BE when introduced replacing next line');
-		elems.key.setAttribute('value', key);
+		if (Object.keys(elems).length < 1) bindElems();
 
-		/*var mapJs = document.createElement('script');
-  mapJs.src = "https://maps.googleapis.com/maps/api/js";
-  mapJs.type = 'text/javascript';
-  document.getElementsByTagName('body')[0].appendChild(mapJs);*/
-
-		var http = function http() {
-			return new Promise(function (resolve, reject) {
-				console.log('please resolve');
-				var url = "https://maps.googleapis.com/maps/api/js";
-				var request = new XMLHttpRequest();
-				request.open('GET', url, true);
-				request.onload = function () {
-					console.log('onload');
-					if (request.status >= 200 && request.status <= 400) {
-						console.log('res', request.response);
-						resolve(request.response);
-					}
-				};
-				request.send();
-			});
-		};
-
-		http().then(function (d) {
-			console.log('here');
-			console.log(d);
-			//initMap();
-		});
-	};
-
-	var initMap = function initMap() {
 		config.eventLatLng = new google.maps.LatLng(51.5719277, 0.420906);
 		var mapOptions = {
 			center: config.eventLatLng,
 			zoom: 17,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-		map = new google.maps.Map(elems.map, mapOptions);
+		var mapObj = new google.maps.Map(elems.map, mapOptions);
 		var iconBase = '/img/icons/';
 		var marker = new google.maps.Marker({
 			position: config.eventLatLng,
-			map: map,
-			icon: iconBase + 'temp-map-marker.png'
+			map: mapObj,
+			icon: iconBase + ' temp-map-marker.png'
 		});
 	};
 
 	var init = function init() {
 		bindElems();
-		fetchApiKey();
 	};
 
 	return {
-		init: init
+		init: init,
+		setupMap: setupMap
 	};
 }();
 muppet.map.init();
--- app/js/map.babel.js
+++ app/js/map.babel.js
@@ -2,71 +2,45 @@
 muppet.map = (() => {
 
 	// config
-	let elems = {};
-	let config = {};
+	const elems = {};
+	const config = {};
 
-	// private methods
-	let bindElems = () => {
+	/**************************************************************************/
+
+	/* PRIVATE METHODS */
+
+	// bind DOM elements to the elems object
+	const bindElems = () => {
 		elems.map = document.getElementById('map');
 		elems.key = document.getElementById('key');
 	};
 
-	let fetchApiKey = () => {
+    const setupMap = () => {
 
-    	console.log('ajax call to return key from BE when introduced replacing next line');
-    	elems.key.setAttribute('value', key);
+    	if (Object.keys(elems).length < 1) bindElems();
 
-    	/*var mapJs = document.createElement('script');
-    	mapJs.src = "https://maps.googleapis.com/maps/api/js";
-    	mapJs.type = 'text/javascript';
-    	document.getElementsByTagName('body')[0].appendChild(mapJs);*/
-
-    	let http = function() {
-    		return new Promise((resolve, reject) => {
-	    		let url = "https://maps.googleapis.com/maps/api/js";
-	    		let request = new XMLHttpRequest();
-	    		request.open('GET', url, true);
-	    		request.onload = () => {
-	    			console.log('onload')
-	    			if (request.status >= 200 && request.status <= 400) {
-	    				console.log('res', request.response);
-	    				resolve(request.response);
-	    			}
-	    		}
-	    		request.send();
-	    	});
-	    };
-
-	    http().then(function(d){
-	    	console.log('here');
-	    	console.log(d);
-	    	//initMap();
-	    });
-    };
-
-    let initMap = () => {
     	config.eventLatLng = new google.maps.LatLng(51.5719277,0.420906);
-    	var mapOptions = {
+    	const mapOptions = {
     		center: config.eventLatLng,
     		zoom:17,
     		mapTypeId: google.maps.MapTypeId.ROADMAP
     	};
-    	map = new google.maps.Map(elems.map, mapOptions);
-    	var iconBase = '/img/icons/';
-		var marker = new google.maps.Marker({
+    	const mapObj = new google.maps.Map(elems.map, mapOptions);
+    	const iconBase = '/img/icons/';
+		const marker = new google.maps.Marker({
 		  position: config.eventLatLng,
-		  map: map,
-		  icon: iconBase + 'temp-map-marker.png'
+		  map: mapObj,
+		  icon: `${iconBase} temp-map-marker.png`
 		});
     };
 
-	let init = () => {
+	const init = () => {
 		bindElems();
-		fetchApiKey();
 	};
 
 	return {
-		init: init
+		init: init,
+		setupMap: setupMap
 	}
 
 })();
