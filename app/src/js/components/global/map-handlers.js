d5cdd83e315f7b4958c918c0bc5d6fb6361c40a1
Phil Gibbins <scoobster17@hotmail.co.uk>
2017-02-08 14:43:06 +0000

    Committing stuff found on a machine wanting to remove project from

--- app/src/js/components/global/map-handlers.js
+++ app/src/js/components/global/map-handlers.js
@@ -20,6 +20,26 @@ export function getIcons (mapPageComponent, map, maps) {
     })
 }
 
+export function getPlaces (mapPageComponent, map, maps) {
+
+    const placesRequest = new Promise((resolve, reject) => {
+        var placesJSON = document.createElement('script');
+        mapIconsScript.onload = function() {
+            if (typeof MAP_PIN !== 'undefined') {
+                resolve('Success getting map icons');
+            } else {
+                reject('Failed to get map icons');
+            }
+        }
+        mapIconsScript.src = "/bower_components/map-icons/dist/js/map-icons.min.js";
+        document.body.appendChild(mapIconsScript);*/
+        resolve('forced success pending new icons');
+    });
+    mapIconsRequest.then(() => {
+        getLocation(mapPageComponent, map, maps);
+    })
+}
+
 export function reCentreMap (pos, map, maps) {
     map.setCenter(pos);
     map.setZoom(17);
